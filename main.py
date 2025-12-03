1bbe98f650023a0d8190842f8f3021a54f9a43dc
Chintan Patel <chintanpatel02407@gmail.com>
2025-12-03 11:50:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,3 +24,16 @@ def banner():
     print("  |__/                                 ")
     print("\n\tTwitter: " + Fore.MAGENTA + "@blackeko5")
 
+
+def getUserId(username, sessionsId):
+    cookies = {'sessionid': sessionsId}
+    headers = {'User-Agent': 'Instagram 64.0.0.14.96', }
+    r = get('https://www.instagram.com/{}/?__a=1'.format(username),
+            headers=headers, cookies=cookies)
+    try:
+        info = json.loads(r.text)
+        id = info["logging_page_id"].strip("profilePage_")
+        return({"id": id, "error": None})
+    except:
+        return({"id": None, "error": "User not found or rate limit"})
+
